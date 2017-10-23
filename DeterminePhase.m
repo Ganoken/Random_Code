94a9be450df94d3f79e7f193772f5b1a8d8bdea6
JP7FGD <jp7fgd@outlook.com>
2017-10-24 00:26:14 +0900

    Update

--- DeterminePhase.m
+++ DeterminePhase.m
@@ -1,15 +1,16 @@
 %Find peaks and make time stamp for phase mean
+%Sujoy created 2017/10
 
 %initialize and load data
 Frequency = 100e3;%kHz
-RawPData = csvread('60off.csv');
-DataCount = length(RawPData);
-MaxT = DataCount/Frequency;
-TimeLine = 0:1/Frequency:MaxT-(1/Frequency);
-PhaseDivision = 32; % # of 
+RawPData = csvread(''); %Assuming the data is in csv format
+DataCount = length(RawPData); % Get the # of Data 
+MaxT = DataCount/Frequency; % Get the duration of data
+TimeLine = 0:1/Frequency:MaxT-(1/Frequency); % Generate the time vactor for the Data
+PhaseDivision = 32; % # of phase 
 
 %Make Filter if needed
-frequncylow=[80 149]; % lower side 
+frequncylow=[80 149]; % lower side for multiple filter increase the element
 frequncyhigh=[220 151]; % higher side
 filter1 = generatefilter(0,frequncylow(1),frequncyhigh(1),Frequency); % 0:bandpass, 1:bandstop
 filter2 = generatefilter(1,frequncylow(2),frequncyhigh(2),Frequency); 
@@ -18,14 +19,14 @@ filter2 = generatefilter(1,frequncylow(2),frequncyhigh(2),Frequency);
 FilteredData1 = ApplyFilter (filter1,RawPdata); % pass the filter and data
 FilteredData2 = ApplyFilter (filter2,FilteredData1); % increase as many times desired
 
-%Check 
+%Check whether desired filtered signal is otained
 % freq comp
 % pwr
 
 %Peak Detection
 [Peaks,TimeStamps] = findpeaks(FilteredData2,TimeLine,'MinPeakDistance',0.0045);
 
-
+%Elemenate initial large peaks if needed
 
 
 
