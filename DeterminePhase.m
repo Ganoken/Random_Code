e92ebc5d61e65001c2777e18194a2044e92c0d70
JP7FGD <jp7fgd@outlook.com>
2017-10-24 18:47:17 +0900

    Update and add

--- DeterminePhase.m
+++ DeterminePhase.m
@@ -1,7 +1,7 @@
 %Find peaks and make time stamp for phase mean
 %Sujoy created 2017/10
 
-%initialize and load data
+%% initialize and load data
 Frequency = 100e3;%kHz
 RawPData = csvread(''); %Assuming the data is in csv format
 DataCount = length(RawPData); % Get the # of Data 
@@ -9,26 +9,43 @@ MaxT = DataCount/Frequency; % Get the duration of data
 TimeLine = 0:1/Frequency:MaxT-(1/Frequency); % Generate the time vactor for the Data
 PhaseDivision = 32; % # of phase 
 
-%Make Filter if needed
+%% Make Filter if needed
 frequncylow=[80 149]; % lower side for multiple filter increase the element
 frequncyhigh=[220 151]; % higher side
 filter1 = generatefilter(0,frequncylow(1),frequncyhigh(1),Frequency); % 0:bandpass, 1:bandstop
 filter2 = generatefilter(1,frequncylow(2),frequncyhigh(2),Frequency); 
 
-%Apply filter if needed
+%% Apply filter if needed
 FilteredData1 = ApplyFilter (filter1,RawPdata); % pass the filter and data
 FilteredData2 = ApplyFilter (filter2,FilteredData1); % increase as many times desired
 
-%Check whether desired filtered signal is otained
+%% Check whether desired filtered signal is otained
 % freq comp
 % pwr
 
-%Peak Detection
+%% Peak Detection
 [Peaks,TimeStamps] = findpeaks(FilteredData2,TimeLine,'MinPeakDistance',0.0045);
+%Take the desired length
 
-%Elemenate initial large peaks if needed
+%% Elemenate initial large peaks if needed
+%It will be later modified to use statistical treatment
+%Currently manual picking of value
+%% caution temporary 
+%TimeDuration = 2 + TimeStamps(1,15);
+%TimeUse = TimeStamps(TimeStamps < TimeDuration); % need two sec
+%CleanedTimeStamps = TimeUse(15:end);% manually selected
+
+CleanedTimeStamps = TimeStamps(15:end);
+ShiftedTime = CleanedTimeStamps -TimeStamps(1,15);
+TimeUse = ShiftedTime(1:1+nnz(ShiftedTime < 2));
 
 
+%% generate the time vector for phase mean
 
-
-
+%TotalPhasePoint = length(CleanedTimeStamps)*PhaseDivision;
+s=1;
+PhaseTimeStamps =zeros(1,1);
+for j =1:PhaseDivision:((length(TimeUse)-1)*PhaseDivision)
+    PhaseTimeStamps(j) = TimeUse(1,s);
+    
+end
--- peaktest.m
+++ peaktest.m
@@ -9,4 +9,7 @@ TimeLine = 0:1/Frequency:MaxT-(1/Frequency);
 %[Peaks,TimeStamps] = findpeaks(RawPData,TimeLine,'MinPeakDistance',0.005,'MinPeakHeight',0.2);
 [Peaks,TimeStamps] = findpeaks(bsP,TimeLine,'MinPeakDistance',0.0045);
 
+%B = Peaks>0.02;
+%n = nnz(B);
+
 
