09c5491b8e0ee59fa2889f737a8c668ec46b7e11
JP7FGD <jp7fgd@outlook.com>
2017-11-01 16:53:55 +0900

    Update

--- DeterminePhase.m
+++ DeterminePhase.m
@@ -3,7 +3,6 @@
 
 %% initialize and load data
 Frequency = 100e3;%kHz sampling freq
-RawPData = csvread(''); %Assuming the data is in csv format
 DataCount = length(RawPData); % Get the # of Data 
 MaxT = DataCount/Frequency; % Get the duration of data
 TimeLine = 0:1/Frequency:MaxT-(1/Frequency); % Generate the time vactor for the Data
@@ -16,7 +15,7 @@ filter1 = generatefilter(0,frequncylow(1),frequncyhigh(1),Frequency); % 0:bandpa
 filter2 = generatefilter(1,frequncylow(2),frequncyhigh(2),Frequency); 
 
 %% Apply filter if needed
-FilteredData1 = ApplyFilter (filter1,RawPdata); % pass the filter and data
+FilteredData1 = ApplyFilter (filter1,RawPData); % pass the filter and data
 FilteredData2 = ApplyFilter (filter2,FilteredData1); % increase as many times desired
 
 %% Check whether desired filtered signal is otained
@@ -45,8 +44,33 @@ TimeUse = ShiftedTime(1:1+nnz(ShiftedTime < 2)); % Need 2 sec in this case
 %TotalPhasePoint = length(CleanedTimeStamps)*PhaseDivision;
 s=1;
 
-PhaseTimeStamps =zeros(length(TimeUse)*PhaseDivision,1);
+PhaseTimeStamps =zeros((length(TimeUse)-1)*PhaseDivision,1);
 for j =1:PhaseDivision:((length(TimeUse)-1)*PhaseDivision)
     PhaseTimeStamps(j) = TimeUse(1,s);
+    PhaseDuration = (TimeUse(1,s+1)-TimeUse(1,s))/PhaseDivision;
+    for k = 1:PhaseDivision-1
+        PhaseTimeStamps(j+k) = PhaseTimeStamps(j+k-1)+PhaseDuration;
+        if PhaseTimeStamps(j+k) > 2
+            
+        end
+    end    
     s = s+1;
 end
+
+
+%other implementation
+% s=1;
+% 
+% PhaseTimeStamps =zeros(length(TimeUse)*PhaseDivision,1);
+% for j =1:PhaseDivision:((length(TimeUse)-1)*PhaseDivision)
+%     PhaseTimeStamps(j) = TimeUse(1,s);
+%     PhaseDuration = (TimeUse(1,s+1)-TimeUse(1,s))/PhaseDivision;
+%     for k = 1:PhaseDivision
+%         PhaseTimeStamps(j+k) = PhaseTimeStamps(j)+k*PhaseDuration;
+%         if j+k+1>length(TimeUse)*PhaseDivision 
+%            break
+%        end
+%         
+%     end    
+%     s = s+1;
+% end
