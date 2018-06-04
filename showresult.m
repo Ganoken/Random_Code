da8f9fbe131fd8c2127a3b4c8ec94c9dce9c0d90
JP7FGD <jp7fgd@outlook.com>
2018-06-04 14:26:19 +0900

    update

--- showresult.m
+++ showresult.m
@@ -1,14 +1,17 @@
 
-load('../../Output/MeanImage/20170104L_75_on_1_201701_1147mean.mat')
+%load('../Test/chem_nor_mean.mat')
 V = 0.0:0.1:1.0;
-X = 480:800;
-Y = 400:700;
-for n=1:32
-    contourf(X,Y,MeanImage(:,:,n),V)
+X = 50:500;
+Y = 100:650;
+for n=1:9
+    %contourf(X,fliplr(Y),Nor_mean2(:,:,n),V)
+    contourf(X,fliplr(Y),Nor_mean2(:,:,n))
     set(gca, 'FontName','Times','FontSize',16 );
+    %axis off;
     caxis([0 1]);
-    colormap hot
-    colorbar ('TickLabels', {'0.0', '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1.0'});
-    FileName = ['../../Output/MeanImage/bmp/75on/Phase' num2str(n,'%02u') '.bmp'];
+    colormap jet
+    %colorbar
+    %colorbar ('TickLabels', {'0.0', '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1.0'});
+    FileName = ['../Output/chem/final/mean_Hz' num2str(n,'%02u') '.bmp'];
     saveas(gcf,FileName)
 end
