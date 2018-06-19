0b2c9b66d2fea7a38ac95e36a951d7766ed5b0c6
JP7FGD <jp7fgd@outlook.com>
2018-06-20 00:26:43 +0900

    update

--- SpecTest2.m
+++ SpecTest2.m
@@ -1,9 +1,9 @@
- S = xlsread('G:/pres/20180608/pressure_14.xlsx','A2:A200001');
+ S = xlsread('E:/JP7FGD/Documents/Lab/Reasearch/pres/20180618/pressure_12.xlsx','A2:A200001');
  Fs = 20e3;
  n = length (S);
  TimeLine = 0:1/Fs:10-(1/Fs);
  PData = 0.5*S;
  [pxx,f] = periodogram(S,hamming(length(S)),32768,Fs,'power');
- loglog(f,pxx,'-sr')
- %loglog(f,pxx,'-s', 'color', [0.3 0.6 0.7])
+ %loglog(f,pxx,'-sc')
+ loglog(f,pxx,'-s', 'color', [0.3 0.6 0.7])
  xlim([10 1000])
--- showresult.m
+++ showresult.m
@@ -1,17 +1,17 @@
 
 %load('../Test/chem_nor_mean.mat')
 V = 0.0:0.1:1.0;
-X = 50:500;
+X = 100:600;
 Y = 100:650;
 for n=1:9
     %contourf(X,fliplr(Y),Nor_mean2(:,:,n),V)
-    contourf(X,fliplr(Y),Nor_mean2(:,:,n))
+    contourf(X,fliplr(Y),Nor_mean(:,:,n))
     set(gca, 'FontName','Times','FontSize',16 );
-    %axis off;
+    axis off;
     caxis([0 1]);
     colormap jet
-    %colorbar
+    colorbar
     %colorbar ('TickLabels', {'0.0', '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1.0'});
-    FileName = ['../Output/chem/final/mean_Hz' num2str(n,'%02u') '.bmp'];
+    FileName = ['I:\ch0618\output\image/mean_0.67_Hz' num2str(n,'%02u') '.bmp'];
     saveas(gcf,FileName)
 end
