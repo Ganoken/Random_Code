1d331fe78adc012b6f8cca975d825323ab3bc5af
JP7FGD <jp7fgd@outlook.com>
2018-07-02 23:58:59 +0900

    update

--- PPP.m
+++ PPP.m
@@ -1,5 +1,5 @@
 %
-S = xlsread('C:/Users/chakraborty.s.ab/Documents/lab/SujoyWork/exp/pres/20180618/pressure_11.xlsx','A2:A200001');
+S = xlsread('I:/pres/20180618/pressure_11.xlsx','A2:A200001');
 
 Frequency = 20e3;
 DataCount = length(S);
@@ -7,33 +7,42 @@ MaxT = DataCount/Frequency;
 TimeLine = 0:1/Frequency:MaxT-(1/Frequency);
 
 PData = 0.5*S(1:40000);
-
+TimeLineS = TimeLine(1:40000);
 windowSize = 20; 
 b = (1/windowSize)*ones(1,windowSize);
 a = 1;
-filtered = filtfilt(b,a,S);
+filtered = filtfilt(b,a,PData);
 
-[Peaks,TimeStamps] = findpeaks(filtered,TimeLine,'MinPeakDistance',0.01);
+%[Peaks,TimeStamps] = findpeaks(filtered,TimeLine,'MinPeakDistance',0.01);
 
- Fs = 20e3;
- nfft = 16384;
-  [pxx, f] = pwelch(PData,kaiser(nfft,0.5),[],nfft,Fs,'power');
-  
-  %  ax = gca;
-%     ax.XAxisLocation = 'bottom';
-%     ax.YDir='normal';
-%     ax.YAxisLocation = 'left';
-%     ax.XColor = 'black';
-%     ax.YColor = 'black';
-%     ax.XScale = 'log';
-%     ax.YScale = 'log';
-%     %ax.XLim = [30 300];
-%    ax.YLim = [10^-9 10^-3];
+ %Fs = 20e3;
+ %nfft = 16384;
+ %[pxx, f] = pwelch(PData,kaiser(nfft,0.5),[],nfft,Fs,'power');
+ %[pxxf, ff] = pwelch(filtered,kaiser(nfft,0.5),[],nfft,Fs,'power');
+ 
+ %z=zeros(1,8193);
+ %z=z-0.4;
+ %plot3(ff,pxxf,z,'-k')
+ %plot3(ff,pxxf,z,'-','color', [1 0.5 0])
+ 
+  scrsz=get(groot,'ScreenSize');
+ figure('Position', [1 scrsz(2)/3 scrsz(3)/2 scrsz(4)/2]);
+ plot(TimeLineS,PData,'-k');
+  ax = gca;
+     ax.XAxisLocation = 'bottom';
+     ax.YDir='normal';
+     ax.YAxisLocation = 'left';
+     ax.XColor = 'black';
+     ax.YColor = 'black';
+ %    ax.XScale = 'log';
+ %    ax.YScale = 'log';
+  %   ax.XLim = [30 300];
+  %  ax.YLim = [10^-9 10^-2];
 % %     ax.XTickMode = 'manual';
 % %     ax.YTickMode = 'manual';
 % %     ax.XTick = 30^0:30^2;
 % %     ax.YTick = 10^-11:10^-3;
-% %     ax.TickDir = 'in';
+  %   ax.TickDir = 'in';
 % %     ax.TickLength = [0.02 0.035];
 % %     ax.XMinorTick = 'on';
 % %     ax.YMinorTick = 'on';
@@ -42,15 +51,15 @@ filtered = filtfilt(b,a,S);
 % %     ax.GridColor = 'yellow';
 % %     ax.GridLineStyle = ':';
 %     % '-'  ':'  '-.' '--'
-%     ax.FontSize = 15;
-%     ax.FontName =  'Times New Roman';
-%     ax.TitleFontSizeMultiplier = 2;
+     ax.FontSize = 24;
+     ax.FontName =  'Times New Roman';
+     ax.TitleFontSizeMultiplier = 2;
 % 
 % %     c.Position = [0.31 0.405 0.01 0.225];
 %  
-%     xlabel('\it \fontname{Times New Roman}f \rm[Hz]');
-%     ylabel('\it \fontname{Times New Roman}Power ');
+     xlabel('\it \fontname{Times New Roman}T \rm[Sec]');
+     ylabel('\it \fontname{Times New Roman}kHz ');
 %  
  %select legend
-%  legend('non-exciting','50Hz','60Hz','70Hz','80Hz','90Hz','100Hz','110Hz','Location','Northeast');
+%  legend('off','50Hz','60Hz','70Hz','80Hz','90Hz','100Hz','110Hz','120Hz','Location','Northeast');
 %  legend('non-exciting','120Hz','130Hz','140Hz','150Hz','160Hz','170Hz','180Hz','Location','Northeast');
